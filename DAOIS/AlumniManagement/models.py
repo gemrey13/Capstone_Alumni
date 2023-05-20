02c94de9e2d68f69015483e0ffeddad05f806a19
gemrey <gemreyranola@gmail.com>
2023-05-20 09:52:33 +0800

    The Alumni can have multiple jobs but a job can only one alumni

--- DAOIS/AlumniManagement/models.py
+++ DAOIS/AlumniManagement/models.py
@@ -1,30 +1,32 @@
 from django.db import models
 
 
+
+
 class Course(models.Model):
     course_id = models.CharField(max_length=6, primary_key=True)
     course_name = models.CharField(max_length=64, blank=False)
     course_desc = models.CharField(max_length=255, blank=True)
 
+class Alumni_Demographic_Profile(models.Model):
+    alumni_id = models.CharField(primary_key=True, max_length=6)
+    fname = models.CharField(max_length=64)
+    lname = models.CharField(max_length=64)
+    mi = models.CharField(max_length=2, blank=True)
+    suffix = models.CharField(max_length=3, blank=True)
+    address = models.CharField(max_length=64)
+    religion = models.CharField(max_length=64)
+    marital_status = models.CharField(max_length=64)
+    date_of_birth = models.CharField(max_length=64)
+
 class Current_Job(models.Model):
-    current_job_id = models.CharField(primary_key=True, max_length=10)
+    current_job_id = models.CharField(primary_key=True, max_length=6)
     job_title = models.CharField(max_length=64)
     salary = models.IntegerField()
     start_date = models.DateField()
     company_name = models.CharField(max_length=64)
     company_address = models.CharField(max_length=64)
-
-class Curriculum(models.Model):
-    curriculum_id = models.CharField(max_length=10, primary_key=True)
-    curriculum_name = models.CharField(max_length=64, blank=False)
-    description = models.CharField(max_length=255, blank=True)
-    year = models.DateField(blank=False)
-
-class Segment(models.Model):
-    segment_id = models.IntegerField(primary_key=True)
-    course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    current_job = models.ForeignKey(Current_Job, on_delete=models.CASCADE)
-    curriculum = models.ForeignKey(Curriculum, on_delete=models.CASCADE)
+    alumni = models.ForeignKey(Alumni_Demographic_Profile, on_delete=models.CASCADE, null=True, blank=True)
 
 class Previous_Job(models.Model):
     previous_job_id = models.CharField(primary_key=True, max_length=10)
@@ -34,22 +36,26 @@ class Previous_Job(models.Model):
     end_date = models.DateField()
     company_name = models.CharField(max_length=64)
     company_address = models.CharField(max_length=64)
+    alumni = models.ForeignKey(Alumni_Demographic_Profile, on_delete=models.CASCADE, null=True, blank=True)
 
-class Alumni_Demographic_Profile(models.Model):
-    alumni_id = models.CharField(primary_key=True, max_length=10)
-    fname = models.CharField(max_length=64)
-    lname = models.CharField(max_length=64)
-    mi = models.CharField(max_length=2)
-    suffix = models.CharField(max_length=3)
-    address = models.CharField(max_length=64)
-    religion = models.CharField(max_length=64)
-    marital_status = models.CharField(max_length=64)
-    date_of_birth = models.CharField(max_length=64)
-    current_job = models.ForeignKey(Current_Job, on_delete=models.SET_NULL, null=True, blank=True)
-    previous_job = models.ForeignKey(Previous_Job, on_delete=models.SET_NULL, null=True, blank=True)
+
+class Curriculum(models.Model):
+    curriculum_id = models.CharField(max_length=10, primary_key=True)
+    curriculum_name = models.CharField(max_length=64, blank=False)
+    description = models.CharField(max_length=255, blank=True)
+    year = models.CharField(max_length=4, blank=False)
+
+
+class Segment(models.Model):
+    segment_id = models.IntegerField(primary_key=True)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    current_job = models.ForeignKey(Current_Job, on_delete=models.CASCADE)
+    curriculum = models.ForeignKey(Curriculum, on_delete=models.CASCADE)
+
+
 
 class Graduate(models.Model):
-    graduate_id = models.CharField(max_length=10, primary_key=True)
+    graduate_id = models.CharField(max_length=6, primary_key=True)
     graduation_date = models.DateField()
     degree = models.CharField(max_length=64)
     honor = models.CharField(max_length=64)
