59c022a153b34a1e3d742f04cf596e327a83de09
gemrey <gemreyranola@gmail.com>
2023-05-21 16:32:12 +0800

    Added New template for dashboard

--- DAOIS/AlumniManagement/urls.py
+++ DAOIS/AlumniManagement/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 
 urlpatterns = [
     path('', views.dashboard),
     path('job/', views.related_job),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
