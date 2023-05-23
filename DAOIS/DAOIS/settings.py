4a49bb42c6ee3712c04a8b726200d0555d7d224f
gemrey <gemreyranola@gmail.com>
2023-05-23 18:41:09 +0800

    New

--- DAOIS/DAOIS/settings.py
+++ DAOIS/DAOIS/settings.py
@@ -118,9 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 import os
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATIC_ROOT = '/home/gemrey/Capstone_Alumni/DAOIS/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
