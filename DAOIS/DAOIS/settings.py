28966cbf5cd17deff29786e253ddeeec998c233b
gemrey <gemreyranola@gmail.com>
2023-05-23 19:25:08 +0800

    .

--- DAOIS/DAOIS/settings.py
+++ DAOIS/DAOIS/settings.py
@@ -120,7 +120,6 @@ import os
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATIC_ROOT = '/home/gemrey/Capstone_Alumni/DAOIS/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
